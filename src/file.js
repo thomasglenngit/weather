0c5ab05a3210585c672206e1a9e0ca4aaf46d1c3
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-01 22:33:06 -0600

    add nested promise .js code.

--- src/file.js
+++ src/file.js
@@ -1,26 +1,58 @@
 import { WeatherService } from './../src/weather-service.js';
+let weatherKey = [WEATHER-KEY-HERE];
+let giphyKey = [GIPHY-KEY-HERE];
 
 $(document).ready(function() {
-
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
+    let city = $('#location').val();
     $('#location').val("");
 
-    (async () => {
-      let weatherService = new WeatherService();
-      const response = await weatherService.getWeatherByCity(city);
-      getElements(response);
-    })();
+    function weatherCall() {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
 
-    function getElements(response) {
-      if (response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-      } else {
-        $('.showHumidity').text(`There was an error handling your request.`);
-        $('.showTemp').text(`Please check your inputs and try again!`);
-      }
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      });
     }
 
+    function giphyCall(humidity) {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url = `http://api.giphy.com/v1/gifs/search?q=${humidity}&api_key=${giphyKey}&limit=5`
+
+        request.onload = function() {
+          if (this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+
+    weatherCall()
+      .then(function(response) {
+        let body = JSON.parse(response);
+        let humidity = body.main.humidity;
+        return giphyCall(humidity);
+      })
+      .then(function(response) {
+      let giphyResponse = JSON.parse(response);
+      let image = giphyResponse["data"][0]["images"]["downsized"]["url"];
+      $('.showImage').html(`<img src='${image}'>`);
+    });
   });
 });
